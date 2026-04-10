442aeb5657d2babfce1fec312fb25b01f53f8bb5
Sanjeet Roy <sanjeet1603@gmail.com>
2026-04-10 12:22:20 +0530

    Add files via upload

---
+++ Top_Interview_150/Day_021.c
@@ -0,0 +1,29 @@
+/*
+    Stack :
+    -> Valid Parentheses (Easy)
+      #20 String | Stack
+*/
+
+#include <string.h>
+
+bool isValid(char *s){
+    char stack[strlen(s)];
+    int top = -1;
+
+    for(int i = 0; s[i] != '\0'; i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack[++top] = s[i];
+        }
+        else{
+            if (top == -1 || 
+               (s[i] == ')' && stack[top] != '(') || 
+               (s[i] == '}' && stack[top] != '{') || 
+               (s[i] == ']' && stack[top] != '[')) {
+                return false;
+               }
+               top--;
+        }
+    }
+
+    return top == -1;
+}
