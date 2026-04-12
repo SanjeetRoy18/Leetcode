d8c30dd0ca079ba62460fb66582e6a33da9e3690
Sanjeet Roy <sanjeet1603@gmail.com>
2026-04-12 12:58:04 +0530

    Add files via upload

---
+++ Top_Interview_150/Day_023_#55.c
@@ -0,0 +1,27 @@
+/*
+    Array/String :
+    -> Jump Game (Medium)
+      #55 | Array | Dynamic Programming | Greedy
+*/
+
+bool canJump(int* nums, int numsSize) {
+
+    int jump = 0;
+
+    for(int i = 0; i<numsSize; i++){
+
+        if(jump < i){
+            break;
+        }
+
+        if(jump < i + nums[i]){
+            jump = i + nums[i];
+        }
+
+        if(jump >= numsSize-1){
+            return true;
+        }
+    }    
+
+    return false;
+}
